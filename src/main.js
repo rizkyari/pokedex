a703121e403bc6b2b1740066351b9c8d6c083a89
rizky.ariananda <rizky.ariananda@privyid.tech>
2023-10-20 15:20:11 +0700

    feature: initialize and setup project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+const pinia = createPinia();
+
+createApp(App).use(pinia).mount('#app')
